17f3762ab652f71ab3acd97567276c61b6b6957b
Charan <charansri795@gmail.com>
2022-12-20 22:58:43 +0530

    updates

--- src/Components/portfolio/items.js
+++ src/Components/portfolio/items.js
@@ -1,17 +1,37 @@
 import Art from '../../assests/art.png'
 import Project from '../../assests/sde.png'
 import Gameproj from '../../assests/game.png'
-import MLproj from '../../assests/mlart.png'
+import detect from '../../assests/detect.jpg'
+import book from '../../assests/bookworms.png'
+import facerec from '../../assests/Facerec.png'
+import cancer from '../../assests/cancer.png'
+import snake from '../../assests/snake.png'
 
 const items=[
     {
       id:1,
-      image: MLproj,
+      image: facerec,
       cat: "Artificial Intelligence",
-      title: "ML Projects",
+      title: "Face Recognition",
       Github: 'https://github.com/CharanSriKrishna/Projects-Machine-Learning',
       demo : 'https://drive.google.com/drive/folders/18DGZSnm2G0DCbs9mnroXHFd2wwKESVSJ?usp=sharing'
     },
+    {
+      id:5,
+      image: detect,
+      cat: "Artificial Intelligence",
+      title: "Object Detection",
+      Github: 'https://github.com/CharanSriKrishna/Projects-Object_Detection_Stone_Paper_Scisssor',
+      demo : 'https://drive.google.com/drive/folders/1PkovVOCP5qSxCnr3vanfwkiQOhzUjK5z?usp=sharing'
+  },
+    {
+      id:6,
+      image: book,
+      cat: "Artificial Intelligence",
+      title: "Bookworm",
+      Github: 'https://github.com/CharanSriKrishna/Projects-OpenAi-Bookworm',
+      demo : 'https://drive.google.com/drive/folders/1NkA-PMsYIG8rqgPgFwZ9JCxoEStqUIsU?usp=sharing'
+  },
     {
       id:2,
       image: Art,
@@ -22,20 +42,22 @@ const items=[
     },
     {
     id:3,
-    image: Gameproj,
+    image: snake,
     cat: "Game Project",
-    title: "Game Project",
-    Github: 'https://github.com/CharanSriKrishna/Projects-Game-Projects',
-    demo : 'https://drive.google.com/drive/folders/1nvT8CB46Lze1B7MdkZRVWZ6IDVQzd0uW?usp=share_link'
+    title: "Dual Snake",
+    Github: 'https://github.com/CharanSriKrishna/Projects-Game-Projects-Snake_Game',
+    demo : 'https://drive.google.com/drive/folders/17m_vX-gUVVgwhyVUEvqepSremiemeEFX?usp=sharing'
     },
     {
     id:4,
-    image: Project,
-    title: "Projects",
-    cat: "Software Project",
-    Github: 'https://github.com/CharanSriKrishna/Projects-Other-Projects',
-    demo : 'https://drive.google.com/drive/folders/1kOWnhZZpnaMvo-wnWk6ReMCUapVlQcLa?usp=share_link'
+    image: cancer,
+    title: "Cancer Prediction",
+    cat: "Artificial Intelligence",
+    Github: 'https://github.com/CharanSriKrishna/Projects-Machine-Learning-Smv-Breast-Cancer',
+    demo : 'https://drive.google.com/drive/folders/1CJchGqU2TYNYwo1zKkgn4Dt_PWZKrRAG?usp=sharing'
       },
+    
+    
   ];
 
 export default items
